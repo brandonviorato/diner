570dc7d5e433341fc3afd78e79b43818bd8275ff
brandonviorato <brandonviorato@outlook.com>
2024-04-11 12:41:33 -0400

    create controller

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// 328/diner/index.php
+// this is my controller
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require
+require_once('vendor/autoload.php');
+
+// instantiate the base F3 class
+$f3 = Base::instance();
+
+// define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Hello diner</h1>';
+
+    // render a view page
+    //$view = new Template();
+    //echo $view->render('views/index.html');
+});
+
+// run fat-free
+$f3->run();

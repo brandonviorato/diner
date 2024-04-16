ef546e63faa1b58af0e20397e126366e0b148d83
brandonviorato <brandonviorato@outlook.com>
2024-04-16 13:50:28 -0400

    Define order 1 route

--- index.php
+++ index.php
@@ -49,5 +49,43 @@ $f3->route('GET /menus/dinner', function() {
     echo $view->render('views/dinner-menu.html');
 });
 
+// Order 1
+$f3->route('GET|POST /Order1', function($f3) {
+    //echo '<h1>Order 1</h1>';
+
+    // If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+//        echo "<p>You got here using the POST method</p>";
+//        var_dump($_POST);
+
+        // Get the data from the post array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        // if the data valid
+        if (true) {
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+        }
+
+    } else {
+        echo "<p>You got here using the GET method</p>";
+    }
+
+    // render a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+// Order 2
+$f3->route('GET /Order2', function() {
+    //echo '<h1>Order 2</h1>';
+
+    // render a view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
+});
+
 // run fat-free
 $f3->run();
